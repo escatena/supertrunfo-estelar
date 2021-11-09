0a8e0d69b8ff46a2af8bb3ca7e8acaa4c6d3da3d
escatena <eslley@gmail.com>
2021-11-09 02:42:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -54,44 +54,25 @@ jogadores.push(jogador);
 
 //Cria a lista de jogadores que estão na sala ou jogando//
 sock.on("pegarNomes", (opa)=>{
-  console.log(opa);
   io.emit("total",todosNomes)
 })
 
+
 //Avisa o jogador que ele está sendo desafiado
-sock.on('match', (adversario) => {
+sock.on('match', (adversario, nome) => {
 var p1 = jogadores.find(jogador => jogador.nome === adversario);
-var p2 = jogadores.find(jogador => jogador.id === sock.id);
+var p2 = jogadores.find(jogador => jogador.nome === nome);
+io.to(p1.id).emit("duelo", nome);
+console.log("o jogador "+p1.nome+ "foi desafiado");
 
-console.log(p1,p2);
-try {
 removeNome(nomes,p1.nome);
-}
-catch {
-  sock.emit("msg", "Houve um problema, recarregue a página")
-}
-finally {
-
-try{
-  removeNome(nomes,p2.nome);
-}
-catch{
-  sock.emit("msg", "Houve um problema, recarregue a página")
-
-}
-
-}
-try {
-io.to(p1.id).emit("duelo", p2.nome);     }
-catch {
-  sock.emit("msg", "Houve um problema, recarregue a página")
-}
-})
+removeNome(nomes,p2.nome);
+});
 
 //Informa que o combate foi recusado
 sock.on('recusa',(a,b) => {
   var p1 = jogadores.find(jogador => jogador.nome === a);
-  var p2 = jogadores.find(jogador => jogador.id === b);
+  var p2 = jogadores.find(jogador => jogador.nome === b);
 
 nomes.push(p1.nome,p2.nome);
 io.emit('salas', nomes);
@@ -102,10 +83,8 @@ p2.sock.emit('mostrarSala', nomes);
 
 //Informa que o jogador voltou do combate e coloca seu nome na lista da sala novamente
 sock.on('retorno',(b) => {
-  var p = jogadores.find(jogador => jogador.id === b);
-
-nomes.push(p.nome);
-io.emit('salas', nomes);
+nomes.push(b);
+io.emit('mostarSala', nomes);
 })
 
 // ===================================Início do jogo =================================//
@@ -114,13 +93,13 @@ sock.on("inicio", (p1,p2) => {
 
 //Identificação dos jogadores do duelo
 var pl1 = jogadores.find(jogador => jogador.nome === p1);
-var pl2 = jogadores.find(jogador => jogador.id === p2);
-var sala = pl1.id+pl2.id;
+var pl2 = jogadores.find(jogador => jogador.nome === p2);
+var sala = pl1.nome+pl2.nome;
 
 pl1.sock.emit('jogando', pl1.nome, pl2.nome, sala);
 pl1.sock.emit('msg',"É o seu turno!");
 pl2.sock.emit('jogando', pl2.nome, pl1.nome, sala);
-pl2.sock.emit('msg',"Aguardando o adversário!");
+pl2.sock.emit('msg',"Aguardando o adversário iniciar o jogo");
 
 pl1.sala = sala;
 pl2.sala = sala;
@@ -144,24 +123,41 @@ sock.emit("salas", nomes);
 function removeNome(arr, value) { 
   nomes = arr.filter(ele => ele !== value); 
   io.emit('salas',nomes);
+  sock.emit('salas',nomes);
+
   }
 // ================================= Desconexão ========================================//
 
 sock.on('disconnect', function() {
   console.log('Got disconnect!');
   try {
+
   var player = jogadores.find(jogador => jogador.id === sock.id);
   var playerJogo = jogadores.filter(jogador => jogador.sala === player.sala)
+  
   playerJogo.forEach( (p) => {
     if (p.id !== sock.id) {
-      io.to(p.id).emit("msg", "O adversário desconectou. Recarregue a página para voltar à sala de espera.")
+      p.sala = [];
+     io.to(p.id).emit("msg", "O adversário desconectou.")
+     io.to(p.id).emit('volta', "opa")
+      
     }
+    jogadores = jogadores.filter(ele => ele !== player); 
+
+    player.nome = [];
+    player.sala = [];
+    player.id = [];
+    player.sock = [];
+
 
   })
 
   todosNomes = todosNomes.filter(ele => ele !== player.nome); 
+  console.log("Jogador "+player.nome+" desconectou.")
   io.emit('total', todosNomes);
   removeNome(nomes,player.nome);
+  console.log("Lista de nomes disponíveis: "+nomes);
+  console.log("Lista de nomes em jogo: "+todosNomes);
   }
   catch {
     sock.emit("msg", "Houve um problema, recarregue a página!")
